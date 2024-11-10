9497a2fe9df8da82470e8477ed615a2b33edf7bc
Triassic <techbyteofficial9@gmail.com>
2024-11-10 18:38:21 +0200

    Add line under license header to please Checkstyle

--- bootstrap/bungeecord/src/main/java/dev/triassic/template/bungee/TemplateBungeeLogger.java
+++ bootstrap/bungeecord/src/main/java/dev/triassic/template/bungee/TemplateBungeeLogger.java
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 package dev.triassic.template.bungee;
 
 import dev.triassic.template.TemplateLogger;
